91e2415a985a43812c03dcc33d9c734caa32e3ba
februarysea <857254373@qq.com>
2018-12-04 10:20:03 +0800

    coding coding

--- coding.py
+++ coding.py
@@ -3,11 +3,15 @@ import tkinter.messagebox
 
 def channelCapacity():
     #matrix
-    label = Label(root, text="hello,world")
+    frame0.pack()
+
+    label = Label(frame0, text="hello,world")
     label.pack()
     print("hello,world")
 
 def uniqueDecodeableCode():
+    frame1.pack()
+
     def onClick():
         str = code.get()
         if str.isdigit():
@@ -19,11 +23,11 @@ def uniqueDecodeableCode():
         else:
             tkinter.messagebox.showinfo("Information", "Please input 0/1 number")
 
-    codeLabel = Label(text="Please input any code:")
-    code = Entry()
-    showLabel1 = Label(text="Unique Decodeable Code:")
-    showLabel2 = Label(text="Wating calculate...")
-    button = Button(text="confirm", command=onClick)
+    codeLabel = Label(frame1, text="Please input any code:")
+    code = Entry(frame1)
+    showLabel1 = Label(frame1, text="Unique Decodeable Code:")
+    showLabel2 = Label(frame1, text="Wating calculate...")
+    button = Button(frame1, text="confirm", command=onClick)
     codeLabel.grid(row=0, column=0)
     code.grid(row=0, column=1)
     button.grid(row=0, column=2, sticky=E) #sticky 贴合方向W,E,S,N
@@ -71,7 +75,19 @@ menubar.add_cascade(label="信源编码方式", menu=fmenu2)
 menubar.add_cascade(label="常用编码", menu=fmenu3)
 menubar.add_cascade(label="综合", menu=fmenu4)
 
-#创建frame来解决不通过窗口间切换 单击后关闭frame再重新打开
+#创建frame来解决不同窗口间切换 单击后关闭frame再重新打开
+frame0 = Frame(height=450,width=200)
+frame1 = Frame(height=450,width=200)
+frame2 = Frame(height=450,width=200)
+frame3 = Frame(height=450,width=200)
+frame4 = Frame(height=450,width=200)
+frame5 = Frame(height=450,width=200)
+frame6 = Frame(height=450,width=200)
+frame7 = Frame(height=450,width=200)
+frame8 = Frame(height=450,width=200)
+frame9 = Frame(height=450,width=200)
+
+
 root.config(menu=menubar)
 root.mainloop()
 
